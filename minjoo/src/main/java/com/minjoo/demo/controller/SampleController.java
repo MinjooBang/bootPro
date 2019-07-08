7a225dd003e220ae7b70b48c676591b8cbc19fb9
MinjooBang <qkdalswn121@gmail.com>
2019-07-08 20:41:57 +0900

    Security설정

---
+++ minjoo/src/main/java/com/minjoo/demo/controller/SampleController.java
@@ -0,0 +1,36 @@
+package com.minjoo.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping("/sample/*")
+@Controller
+public class SampleController {
+	
+	@GetMapping("/all")
+	public void doAll() {
+		
+		log.info("do all access everybody");
+		
+	}
+	
+	@GetMapping("/member")
+	public void doMember() {
+		
+		log.info("logined member");
+		
+	}
+	
+	@GetMapping("/admin")
+	public void doAdmin() {
+		
+		log.info("admin only");
+		
+	}
+
+}//SampleController
+
